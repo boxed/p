71b3ab8a1227df6c126d07c69e2f30deb38a471a
Anders Hovmöller <boxed@killingar.net>
2017-11-23 13:18:53 +0100

    initial

---
+++ p.py
@@ -0,0 +1,72 @@
+import os
+
+
+def find_available_commands():
+    r = []
+    for path in os.environ["PATH"].split(os.pathsep):
+        r.extend(os.listdir(path))
+
+    return [x for x in r if x.startswith('p-')]
+
+
+def alias_project_type(*, cmd_name, cmd, cfg):
+    assert not cmd.startswith(cmd_name + '-')
+    if not cmd.startswith(cmd_name + ' '):
+        return cmd
+    prefix, *command = cmd.split(' ')
+    assert prefix == cmd_name
+    if 'project_type' in cfg:
+        if command and command[0] != cfg['project_type']:
+            command = [cfg['project_type']] + command
+    return ' '.join([prefix] + command)
+
+
+def alias_once(*, cmd_name, cmd, cfg):
+    aliases = {
+        alias_project_type(cmd_name=cmd_name, cmd=k, cfg=cfg): alias_project_type(cmd_name=cmd_name, cmd=v, cfg=cfg)
+        for k, v in cfg.get('aliases', {}).items()
+    }
+    cmd = alias_project_type(cmd_name=cmd_name, cmd=cmd, cfg=cfg)
+    for k, v in aliases.items():
+        if cmd.startswith(k):
+            new_cmd = v + cmd[len(k):]
+            return alias_project_type(cmd_name=cmd_name, cmd=new_cmd, cfg=cfg)
+    return cmd
+
+
+def alias(*, cmd_name, cmd, cfg):
+    new_cmd = alias_once(cmd_name=cmd_name, cmd=cmd, cfg=cfg)
+    if new_cmd == cmd:
+        return cmd
+    else:
+        return alias(cmd_name=cmd_name, cmd=new_cmd, cfg=cfg)
+
+
+def resolve_cmd(*, available_commands, cmd):
+    prefix, *command = cmd.split(' ')
+    for c in reversed(range(1, len(command) + 1)):
+        try_command = '-'.join([prefix] + command[:c])
+        if try_command in available_commands:
+            params = command[c:]
+            return try_command + ' ' + ' '.join(params)
+    if prefix in available_commands:
+        return ' '.join([prefix] + command)
+
+
+def alias_and_resolve(*, cmd_name, cmd, available_commands, cfg):
+    cmd = alias(cmd_name=cmd_name, cmd=cmd, cfg=cfg)
+    return resolve_cmd(available_commands=available_commands, cmd=cmd)
+
+
+class ConfigError(Exception):
+    pass
+
+
+def validate_config(*, cfg):
+    if 'project_type' in cfg and ' ' in cfg['project_type']:
+        raise ConfigError('project_type cannot contain spaces')
+    if 'aliases' in cfg:
+        aliases = cfg['aliases']
+        for a in aliases.keys():
+            if '-' in a:
+                raise ConfigError('Aliases should be written in the form "foo bar", not "fo-bar". Incorrect value "%s"' % a)
---
+++ tests/test_p.py
@@ -0,0 +1,54 @@
+import pytest
+
+from p import validate_config, ConfigError, resolve_cmd, alias_and_resolve, alias, alias_once, alias_project_type
+
+
+def test_alias_project_type():
+    assert alias_project_type(cmd_name='p', cmd='p repl', cfg=dict(project_type='python')) == 'p python repl'
+
+    assert alias_project_type(cmd_name='p', cmd='p python repl', cfg=dict(project_type='python')) == 'p python repl'
+
+    assert alias_project_type(cmd_name='p', cmd='foo bar', cfg={}) == 'foo bar'
+
+
+def test_alias_once1():
+    assert 'p python repl' == alias_once(cmd_name='p', cmd='p repl', cfg=dict(project_type='python'))
+
+
+def test_alias_once2():
+    assert 'p python run manage.py shell' == alias_once(cmd_name='p', cmd='p repl', cfg=dict(project_type='python', aliases={'p python repl': 'p python run manage.py shell'}))
+
+
+def test_alias_once3():
+    assert 'p python run manage.py shell' == alias_once(cmd_name='p', cmd='p repl', cfg=dict(project_type='python', aliases={'p repl': 'p run manage.py shell'}))
+
+
+def test_alias_once4():
+    assert 'python manage.py shell' == alias_once(cmd_name='p', cmd='p python run manage.py shell', cfg=dict(project_type='python', aliases={'p python run': 'python'}))
+
+
+def test_alias():
+    assert 'python manage.py shell' == alias(cmd_name='p', cmd='p repl', cfg=dict(project_type='python', aliases={
+        'p repl': 'p run manage.py shell',
+        'p python run': 'python',
+    }))
+
+
+def test_resolve():
+    assert resolve_cmd(available_commands=['p-new', ], cmd='p new python foo') == 'p-new python foo'
+
+    assert resolve_cmd(available_commands=['p-new', 'p-new-python'], cmd='p new python foo') == 'p-new-python foo'
+
+    assert resolve_cmd(available_commands=[], cmd='p new python foo') is None
+
+
+def test_alias_and_resolve():
+    assert 'python manage.py shell' == alias_and_resolve(cmd_name='p', cmd='p repl', available_commands=['python'], cfg=dict(project_type='python', aliases={'p repl': 'p run manage.py shell', 'p python run': 'python'}))
+
+
+def test_validate_config():
+    with pytest.raises(ConfigError):
+        validate_config(cfg=dict(project_type=' foo bar'))
+
+    with pytest.raises(ConfigError):
+        validate_config(cfg=dict(aliases={'foo-bar': 'bar-baz'}))
